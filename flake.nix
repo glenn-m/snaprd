b5809a7311304918c72e7c27b1e5de1e8c0d27dd
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2023-03-08 09:56:40 -0500

    Updating Nix Flake with working package

--- flake.nix
+++ flake.nix
@@ -3,88 +3,110 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     devshell.url = "github:numtide/devshell";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, flake-utils, devshell, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages = let pkgs = import nixpkgs { inherit system; };
-      in { snaprd = pkgs.callPackage ./snaprd.nix; };
-      nixosModules = {
-        snaprd = { config, lib, pkgs, ... }:
-          with lib;
-          let
-            cfg = config.services.snaprd;
-            mkConfigFile =
-              pkgs.writeText "snaprd.yml" (builtins.toJSON cfg.configuration);
-          in {
-            options = {
-              services.snaprd = {
-                enable = lib.mkEnableOption "snaprd daemon";
+    # Create system-specific outputs for the standard Nix systems
+    # https://github.com/numtide/flake-utils/blob/master/default.nix#L3-L9
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in rec {
+        defaultPackage = packages.snaprd;
+        packages.snaprd = with pkgs;
+          buildGoModule rec {
+            pname = "snaprd";
+            version = "0.1.0";
 
-                package = mkOption {
-                  type = types.package;
-                  default = self.packages.${pkgs.system}.snaprd;
-                  defaultText = literalExpression "pkgs.snaprd";
-                  description = lib.mdDoc ''
-                    Package that should be used for snaprd.
-                  '';
-                };
-
-                port = mkOption {
-                  type = types.int;
-                  default = 3000;
-                  description = lib.mdDoc ''
-                    Port the Prometheus metrics will be exposed on.
-                  '';
-                };
-
-                metricsPath = mkOption {
-                  type = types.str;
-                  default = "/metrics";
-                  description = lib.mdDoc ''
-                    Path the Prometheus metrics will be exposed on.
-                  '';
-                };
-
-                configuration = mkOption {
-                  type = types.nullOr types.attrs;
-                  default = null;
-                  description = lib.mdDoc ''
-                    Snaprd configuration as nix attribute set.
-                  '';
-                };
-              };
+            src = fetchFromGitHub {
+              owner = "glenn-m";
+              repo = pname;
+              rev = "v${version}";
+              sha256 = "sha256-qvgC1SEWYImC5Y36ozX9Hze0JEJFrPd3wNUlc+Ric8E=";
             };
-            config = lib.mkIf cfg.enable {
-              #environment.systemPackages =
-              #  [ self.packages.${pkgs.system}.snaprd ];
-              systemd.services.snaprd = {
-                description = "snaprd daemon";
-                wantedBy = [ "multi-user.target" ];
-                after = "network.target";
-                serviceConfig = {
-                  ExecStart = "${cfg.package}/bin/snaprd"
-                    + "--configFile=${mkConfigFile}"
-                    + "--metricsPort=${cfg.port}"
-                    + "--metricsPath=${cfg.metricsPath}";
-                  Restart = "always";
-                  WorkDirectory = "/tmp";
-                };
-              };
+
+            vendorSha256 =
+              "sha256-TohfQOr3OMV3C4FmmEkuzO8kMCvkRqhFGcYbhJfh/9c=";
+
+            meta = with lib; {
+              description =
+                "Daemon that runs snaprd on a schedule and surfaces Prometheus metrics";
+              homepage = "https://github.com/glenn-m/snaprd";
+              platforms = platforms.linux;
+              license = licenses.mit;
             };
           };
-      };
-      devShell = let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlay ];
+
+        nixosModules = {
+          snaprd = { config, lib, pkgs, ... }:
+            with lib;
+            let
+              cfg = config.services.snaprd;
+              mkConfigFile =
+                pkgs.writeText "snaprd.yml" (builtins.toJSON cfg.configuration);
+            in {
+              options = {
+                services.snaprd = {
+                  enable = lib.mkEnableOption "snaprd daemon";
+
+                  package = mkOption {
+                    type = types.package;
+                    default = self.packages.${pkgs.system}.snaprd;
+                    defaultText = literalExpression "pkgs.snaprd";
+                    description = lib.mdDoc ''
+                      Package that should be used for snaprd.
+                    '';
+                  };
+
+                  port = mkOption {
+                    type = types.int;
+                    default = 3000;
+                    description = lib.mdDoc ''
+                      Port the Prometheus metrics will be exposed on.
+                    '';
+                  };
+
+                  metricsPath = mkOption {
+                    type = types.str;
+                    default = "/metrics";
+                    description = lib.mdDoc ''
+                      Path the Prometheus metrics will be exposed on.
+                    '';
+                  };
+
+                  configuration = mkOption {
+                    type = types.nullOr types.attrs;
+                    default = null;
+                    description = lib.mdDoc ''
+                      Snaprd configuration as nix attribute set.
+                    '';
+                  };
+                };
+              };
+              config = lib.mkIf cfg.enable {
+                systemd.services.snaprd = {
+                  description = "snaprd daemon";
+                  wantedBy = [ "multi-user.target" ];
+                  after = "network.target";
+                  serviceConfig = {
+                    ExecStart = "${cfg.package}/bin/snaprd"
+                      + "--configFile=${mkConfigFile}"
+                      + "--metricsPort=${cfg.port}"
+                      + "--metricsPath=${cfg.metricsPath}";
+                    Restart = "always";
+                    WorkDirectory = "/tmp";
+                  };
+                };
+              };
+            };
         };
-      in pkgs.devshell.mkShell {
-        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-      };
-    });
+        devShell = let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ devshell.overlays.default ];
+          };
+        in pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+      });
 }
